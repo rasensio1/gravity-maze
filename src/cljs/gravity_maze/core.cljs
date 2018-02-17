e23c4926f82c4161f2490ac40cc38e787a219964
Ryan Asensio <ryan.asensio@gmail.com>
2018-02-17 14:26:12 -0600

    Adds functional mode to quil.

--- src/cljs/gravity_maze/core.cljs
+++ src/cljs/gravity_maze/core.cljs
@@ -1,7 +1,8 @@
 (ns gravity-maze.core
   (:require
    [reagent.core :as reagent]
-   [quil.core :as q :include-macros true]))
+   [quil.core :as q :include-macros true]
+   [quil.middleware :as m]))
 
 (defonce app-state (reagent/atom {}))
 
@@ -9,15 +10,25 @@
   [:div
    [:div "Welcome to reagent-figwheel." ]])
 
-(defn draw []
+(defn setup []
+  {:radius 10})
+
+(defn update-state [state]
+  state)
+
+(defn draw [state]
   (q/background 255)
   (q/fill 0)
-  (q/ellipse 56 46 55 55))
+  (let [r (:radius state)]
+    (q/ellipse 56 46 r r)))
 
 (q/defsketch hello
   :draw draw
+  :setup setup
   :host "host"
-  :size [300 300])
+  :size [300 300]
+  :update update-state
+  :middleware [m/fun-mode])
 
 (defn reload []
   (reagent/render [page app-state]

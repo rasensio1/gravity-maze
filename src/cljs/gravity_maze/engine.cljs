26a201c934a26f2c6995460abc20d156810a96e0
Ryan Asensio <ryan.asensio@gmail.com>
2018-02-19 14:10:55 -0600

    Adds #update-vel and test.

--- src/cljs/gravity_maze/engine.cljs
+++ src/cljs/gravity_maze/engine.cljs
@@ -6,17 +6,26 @@
   ;; return updated @app-state
   state)
 
-(defn addv [& args]
+(defn v+ [& args]
   (apply (partial map +) args))
-
-(defn multv [n vec]
+/
+(defn v* [n vec]
   (map #(* n %) vec))
 
+(defn v-div [n vec]
+  (v* (/ 1 n) vec))
+
 (defn update-pos [dt {:keys [pos vel accel] :as el}]
-  (let [velocity-disp (multv dt vel)
-        accel-disp (multv (* 0.5 dt dt) accel)
-        new-pos (addv pos velocity-disp accel-disp)]
+  (let [velocity-disp (v* dt vel)
+        accel-disp (v* (* 0.5 dt dt) accel)
+        new-pos (v+ pos velocity-disp accel-disp)]
     (assoc el :pos new-pos)))
+
+(defn update-vel [dt new-accel {:keys [vel accel] :as el}]
+  (let [avg-accel (v-div 2 (v+ accel new-accel))
+        accel-dv (v* dt avg-accel)
+        new-vel (v+ vel accel-dv)]
+    (assoc el :vel new-vel)))
 
 ;; ;; for each vector
 ;; position += velocity * time_step + ( 0.5 * last_acceleration * time_step^2 )
--- test/cljs/gravity_maze/engine_test.cljs
+++ test/cljs/gravity_maze/engine_test.cljs
@@ -9,8 +9,8 @@
                  :accel [0 0]
                  :fixed false})
 
-(deftest position-test
-  (testing "Updates a position with velocity"
+(deftest update-pos-test
+  (testing "Updates a position with velocity and acceleration"
     (is (= zero-point (eng/update-pos 1 zero-point)))
     (let [init (assoc zero-point :vel [0 1])
           res (assoc zero-point :pos [0 1] :vel [0 1])]
@@ -18,4 +18,10 @@
     (let [init (assoc zero-point :vel [1 1] :accel [1 1])
           res (assoc zero-point :pos [1.5 1.5] :vel [1 1] :accel [1 1])]
       (is (= res (eng/update-pos 1 init))))))
+
+(deftest update-vel-test
+  (testing "updates the velocity with acceleration"
+    (is (= zero-point (eng/update-vel 1 [0 0] zero-point)))
+    (is (= {:vel [1 1] :accel [2 2]}
+           (eng/update-vel 1 [0 0] {:vel [0 0 ] :accel [2 2]})))))
 

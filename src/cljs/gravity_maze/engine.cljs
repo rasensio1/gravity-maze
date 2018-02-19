97dcecd9356922277c5798a4390e3b3a199c159d
Ryan Asensio <ryan.asensio@gmail.com>
2018-02-19 13:42:38 -0600

    Adds #update-pos and test.

--- src/cljs/gravity_maze/engine.cljs
+++ src/cljs/gravity_maze/engine.cljs
@@ -5,3 +5,21 @@
   ;; swap with app-state atom
   ;; return updated @app-state
   state)
+
+(defn addv [& args]
+  (apply (partial map +) args))
+
+(defn multv [n vec]
+  (map #(* n %) vec))
+
+(defn update-pos [dt {:keys [pos vel accel] :as el}]
+  (let [velocity-disp (multv dt vel)
+        accel-disp (multv (* 0.5 dt dt) accel)
+        new-pos (addv pos velocity-disp accel-disp)]
+    (assoc el :pos new-pos)))
+
+;; ;; for each vector
+;; position += velocity * time_step + ( 0.5 * last_acceleration * time_step^2 )
+;; new_acceleration = force / mass 
+;; avg_acceleration = ( last_acceleration + new_acceleration ) / 2
+;; velocity += avg_acceleration * time_step
--- test/cljs/gravity_maze/engine_test.cljs
+++ test/cljs/gravity_maze/engine_test.cljs
@@ -2,6 +2,20 @@
   (:require [gravity-maze.engine :as eng]
             [cljs.test :refer-macros [deftest testing is]]))
 
+(def zero-point {:type :point
+                 :mass 0
+                 :pos [0 0]
+                 :vel [0 0]
+                 :accel [0 0]
+                 :fixed false})
+
 (deftest position-test
-  (testing "fake"
-    (is (= 1 1))))
+  (testing "Updates a position with velocity"
+    (is (= zero-point (eng/update-pos 1 zero-point)))
+    (let [init (assoc zero-point :vel [0 1])
+          res (assoc zero-point :pos [0 1] :vel [0 1])]
+      (is (= res (eng/update-pos 1 init))))
+    (let [init (assoc zero-point :vel [1 1] :accel [1 1])
+          res (assoc zero-point :pos [1.5 1.5] :vel [1 1] :accel [1 1])]
+      (is (= res (eng/update-pos 1 init))))))
+

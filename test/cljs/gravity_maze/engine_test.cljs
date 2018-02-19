f541991349b5e0e16c5437655e544eddf622cde4
Ryan Asensio <ryan.asensio@gmail.com>
2018-02-19 11:33:52 -0600

    Adds engine-test ns.

---
+++ test/cljs/gravity_maze/engine_test.cljs
@@ -0,0 +1,7 @@
+(ns gravity-maze.engine-test
+  (:require [gravity-maze.engine :as eng]
+            [cljs.test :refer-macros [deftest testing is]]))
+
+(deftest position-test
+  (testing "fake"
+    (is (= 1 1))))
--- test/cljs/gravity_maze/runner.cljs
+++ test/cljs/gravity_maze/runner.cljs
@@ -1,5 +1,7 @@
 (ns gravity-maze.runner
     (:require [doo.runner :refer-macros [doo-tests]]
-              [gravity-maze.core-test]))
+              [gravity-maze.core-test]
+              [gravity-maze.engine-test]))
 
-(doo-tests 'gravity-maze.core-test)
+(doo-tests 'gravity-maze.core-test
+           'gravity-maze.engine-test)
